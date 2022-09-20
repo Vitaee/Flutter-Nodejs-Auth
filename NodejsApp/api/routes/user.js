55cbadbad4dd533ca60a697d9f671d525a0624b8
Can İlgu <canilguu@gmail.com>
2022-09-20 17:42:27 +0300

    deleted import

--- NodejsApp/api/routes/user.js
+++ NodejsApp/api/routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/user');
-const duplicateEmail = require("../middleware/auth/checkEmail");
 
 
 router.post("/",userController.register);
