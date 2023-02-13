74f66b0d3aa4424df83111c0d423c2a30c2d2350
Can İlgu <canilguu@gmail.com>
2023-02-13 19:17:51 +0300

    get fav. code style improved.

--- NodejsApp/api/controllers/favourites/getFav.js
+++ NodejsApp/api/controllers/favourites/getFav.js
@@ -6,26 +6,42 @@ import * as dotenv from 'dotenv'
 import mongoose from 'mongoose';
 dotenv.config()
 
-export default async (req,res) => {
+export default async (req, res) => {
   try {
-        const PAGE_SIZE = 10;                   
-        let skip;
-    
-        req.query.page ? skip = (req.query.page - 1) * PAGE_SIZE : skip = (1 - 1) * PAGE_SIZE;
+    const PAGE_SIZE = 10;
+    const authorization = req.get('authorization');
 
-        let str = req.get('authorization').split(" ")[1];
-    
-        const data =  jsonwebtoken.verify(str, process.env.JWT_SECRET_KEY );
+    if (!authorization) {
+      return res.status(400).send({
+        error: 'Authorization header is missing',
+      });
+    }
 
-        let favs = await favModel.find({"owner":mongoose.Types.ObjectId(data._id)}).populate('owner').sort( {_id: -1} ).skip( skip ).limit( PAGE_SIZE).catch( (e) => {
-          console.log(e)
-          console.log(e.message)
-            return res.status(500).send(errorJson(e, 'An interval server error occurred while getting favs. from db.'))
+    const str = authorization.split(" ")[1];
+    const data = jsonwebtoken.verify(str, process.env.JWT_SECRET_KEY);
+
+    const page = req.query.page || 1;
+    const skip = (page - 1) * PAGE_SIZE;
+
+    const favs = await favModel.find({ owner: mongoose.Types.ObjectId(data._id) })
+      .populate('owner')
+      .sort({ _id: -1 })
+      .skip(skip)
+      .limit(PAGE_SIZE)
+      .catch((e) => {
+        console.error(e);
+        return res.status(500).send({
+          error: 'An internal server error occurred while getting favs from the database',
         });
+      });
 
-        favs.length >= 1 ? res.status(200).send( favs  ) : res.status(404).send([])
-
+    return res.status(200).send({
+      data: favs,
+    });
   } catch (err) {
-    return res.status(500).send(errorJson(err, 'An interval server error occurred'))
+    console.error(err);
+    return res.status(500).send({
+      error: 'An internal server error occurred',
+    });
   }
-}
+};
